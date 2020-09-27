d40af5d200ed2894f26545e6062de1917224f69a
Yojan Stiben Hungría <yshungria@unal.edu.co>
2020-09-27 11:34:02 -0500

    Nuevos cambios

--- archivos_base_javascript_avanzado_expresiones_regulares/05_Casos_comunes/03_Otros_casos/app.js
+++ archivos_base_javascript_avanzado_expresiones_regulares/05_Casos_comunes/03_Otros_casos/app.js
@@ -1,7 +1,7 @@
-$(function () {
+$(function() {
     var contactForm = document.getElementById('contactForm');
 
-    contactForm.onsubmit = function (e) {
+    contactForm.onsubmit = function(e) {
         e.preventDefault();
 
         console.log('¿La URL es válida?', isValidUrl());
@@ -10,10 +10,12 @@ $(function () {
     /**
      * URL/Dirección web válida
      */
-    var isValidUrl = function () {
+    var isValidUrl = function() {
         var url = document.getElementById('url');
         return /^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \?=.-]*)*\/?$/.test(url.value)
     }
+
+
 
     /**
      * Contraseña segura
@@ -22,7 +24,7 @@ $(function () {
      * Contraseñas con al menos un número o caracter especial.
      * Contraseñas con mínimo 8 caracteres.
      **/
-    var isValidPass = function () {
+    var isValidPass = function() {
         var pass = document.getElementById('pass');
         return /(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/.test(pass.value)
     }
@@ -31,7 +33,7 @@ $(function () {
      * Teléfonos en el mundo
      * https://en.wikipedia.org/wiki/List_of_country_calling_codes
      */
-    var isValidPhone = function () {
+    var isValidPhone = function() {
         var phone = document.getElementById('phone');
         return /(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/.test(phone.value)
     }
@@ -39,7 +41,7 @@ $(function () {
     /**
      * Tarjetas de credito
      */
-    var isValidCreditCard = function () {
+    var isValidCreditCard = function() {
         var phone = document.getElementById('phone');
         return /^((67\d{2})|(4\d{3})|(5[1-5]\d{2})|(6011))(-?\s?\d{4}){3}|(3[4,7])\ d{2}-?\s?\d{6}-?\s?\d{5}$/.test(phone.value)
     }
@@ -47,7 +49,7 @@ $(function () {
     /**
      * Código postal
      */
-    var isValidPostalCode = function () {
+    var isValidPostalCode = function() {
         var phone = document.getElementById('phone');
         return /^([1-9]{2}|[0-9][1-9]|[1-9][0-9])[0-9]{3}$/.test(phone.value)
     }
@@ -57,7 +59,7 @@ $(function () {
      * Mínimo 6 caracteres
      * Máximo 30 caracteres
      */
-    var isValidUserName = function () {
+    var isValidUserName = function() {
         var phone = document.getElementById('phone');
         return /^[a-z\d_]{6,30}$/i.test(phone.value)
     }
